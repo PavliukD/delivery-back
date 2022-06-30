3869379c04f334fc4487769482acfe651737e97c
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2022-06-30 13:50:00 +0300

    Update getAllProducts.js

--- controllers/products/getAllProducts.js
+++ controllers/products/getAllProducts.js
@@ -2,7 +2,24 @@ const {Product} = require('../../model')
 
 const getAllProducts = async (req, res, next) => {
     try {
-        const data = await Product.find({})
+        const response = await Product.find({})
+        let shopList = []
+
+        const tmp = response.map(el => {
+            const res = shopList.find(shop => shop === el.shopName)
+            if (res){
+                return
+            }
+            shopList.push(el.shopName)
+            return
+        })
+
+        const data = {
+            shopList,
+            productsList: response
+        }
+
+        console.log(shopList)
 
         res.json({
             status: "success",
